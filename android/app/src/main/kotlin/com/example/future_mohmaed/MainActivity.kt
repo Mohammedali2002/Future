376179d1ea445ff37c8469dacbeb93c442c9e836
Mohammedali2002 <mohammedali81.098@gmail.com>
2022-12-08 01:40:28 +0300

    Future code

---
+++ android/app/src/main/kotlin/com/example/future_mohmaed/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.future_mohmaed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+
+ import 'package:flutter/cupertino.dart';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+void main(List<String> args) => runApp(MaterialApp(home: HomePage()));
+  var count = 0;
+
+class HomePage extends StatelessWidget{
+@override
+  Widget build(BuildContext context) {
+   
+   return Scaffold(
+    body: Center(
+      child: FutureBuilder(
+       future: showName('mohammed'),
+       builder: (context, snapshot) {
+        if(snapshot.data != null) {
+         return Text(snapshot.requireData);
+       }else{
+        return CircularProgressIndicator();
+       }
+       },
+      ),
+       ),
+   );
+  }
+  Future<String> showName(String name) async{
+  await Future.delayed(Duration(seconds: 5));
+  return "my name is : " + name;
+}
+Stream<int> counter()async*{
+  while (count <= 100){
+    await Future.delayed(Duration(seconds: 1));
+  }
+}
+ }
+
